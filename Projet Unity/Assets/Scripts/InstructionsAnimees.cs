11e2c3f8b5c6b5bc31e285cb8ab2d9df9db5ae57
Valentin Francois <valentin.francois35@gmail.com>
2016-12-02 01:08:51 +0100

    Ajout scène d'instructions
    Le bouton 'jouer' du menu pointe maintenant vers la scène d'instructions
    ('barre espace pour sauter, flèches haut-bas pour se déplacer") qui
    lance le jeu (scène JouerTest pour l'instant) dès qu'on appuie sur une
    touche.

---
+++ Projet Unity/Assets/Scripts/InstructionsAnimees.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstructionsAnimees : MonoBehaviour {
+	GameObject espace, flecheH, flecheB;
+	CanvasGroup espaceCanvas, flecheHCanvas, flecheBCanvas;
+	int interval = 0;
+	Animator animator1;
+	bool bas = true;
+	bool deplacer;
+	GameObject perso;
+	int pos = 0;
+
+	// Use this for initialization
+	void Start () {
+		animator1 = transform.Find("SautPersonnage").GetComponent<Animator>();
+		perso = GameObject.Find("DeplacePersonnage");
+		pos = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		interval ++;
+		espaceCanvas = transform.Find("Barre Espace").GetComponent<CanvasGroup>();		
+		flecheHCanvas = transform.Find("Flèche haut").GetComponent<CanvasGroup>();	
+		flecheBCanvas = transform.Find("Flèche bas").GetComponent<CanvasGroup>();			
+		if ((interval >= 80 && interval <100)||(interval >= 180 && interval <200)){
+			espaceCanvas.alpha = 1f;
+		}
+		if (interval == 100){
+			espaceCanvas.alpha = 0.5f;
+		}
+		if (interval == 80 || interval == 180){
+			animator1.SetBool("Sauter", true);
+			deplacer = true;
+			
+		}
+		if (interval==81 || interval == 181){
+			animator1.SetBool("Sauter", false);
+		}
+		if (interval >= 80 && interval <100){
+			flecheHCanvas.alpha = 1f;
+		}
+		if (interval == 100){
+			flecheHCanvas.alpha=0.5f;
+		}
+		if (interval>=180 && interval < 200){
+			flecheBCanvas.alpha=1f;
+		}
+		if (interval == 200){
+			interval = 0;
+			flecheBCanvas.alpha=0.5f;
+			espaceCanvas.alpha = 0.5f;
+		}
+		if (deplacer){
+			if (bas){
+				perso.transform.position = new Vector3(perso.transform.position.x - 2, perso.transform.position.y + 2, perso.transform.position.z);
+				pos++;
+				if (pos == 18){
+					pos = 0;
+					bas = false;
+					deplacer = false;
+				}
+			}
+			else {
+				perso.transform.position = new Vector3(perso.transform.position.x + 2, perso.transform.position.y - 2, perso.transform.position.z);
+				pos++;
+				if (pos == 18){
+					pos = 0;
+					bas = true;
+					deplacer = false;
+				}
+			}
+		}
+	}
+}
---
+++ Projet Unity/Assets/Scripts/TextFadeInterval.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextFadeInterval : MonoBehaviour {
+	int fade = 0;
+	// Use this for initialization
+	void Start () {
+		CanvasGroup canvas = GetComponent<CanvasGroup>();
+		canvas.alpha = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CanvasGroup canvas = GetComponent<CanvasGroup>();
+		if (canvas.alpha < 1 && fade == 0){
+			canvas.alpha += Time.deltaTime / 1;
+			if (canvas.alpha >= 1){
+				canvas.alpha = 1;
+				fade = 1;
+			}
+			
+		}
+		if (canvas.alpha > 0 && fade == 1){
+			canvas.alpha -= Time.deltaTime / 1;
+			if (canvas.alpha <= 0){
+				canvas.alpha = 0;
+				fade = 0;
+			}
+		}
+	}
+}
