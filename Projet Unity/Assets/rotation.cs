91216e7c73acf07f89a303766ba51b7d90348b54
tauchfre <fred.tauch@hotmail.fr>
2016-12-15 13:55:16 +0100

    prefab note qui tourne

---
+++ Projet Unity/Assets/rotation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotation : MonoBehaviour {
+	// Use this for initialization
+	private float y; 
+	private float i; 
+
+	private float k; 
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		y+=4;
+		i -= 0.1f;
+		this.transform.rotation = Quaternion.Euler(0,y,0);  
+		this.transform.position = new Vector3(i,-2, -4 );
+
+		if (transform.position.x < -20) {
+			Destroy (this.gameObject);
+		}
+	}
+}
